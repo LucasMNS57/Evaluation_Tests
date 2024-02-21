a9af51e467d7426c289b9665fc8d2e3c6f751b56
LucasMNS57 <lucasgremese@gmail.com>
2024-02-21 16:15:53 +0100

    rename test project

--- PratiqueWithCode_Groupe2.UnitTest/obj/Debug/net8.0/EvaluationSample.UnitTest.AssemblyInfo.cs
+++ PratiqueWithCode_Groupe2.UnitTest/obj/Debug/net8.0/EvaluationSample.UnitTest.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("EvaluationSampleCode")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("EvaluationSample.UnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b79d415478f56139a689e2e6c3b876a512a0c6b8")]
-[assembly: System.Reflection.AssemblyProductAttribute("EvaluationSampleCode")]
-[assembly: System.Reflection.AssemblyTitleAttribute("EvaluationSampleCode")]
+[assembly: System.Reflection.AssemblyProductAttribute("EvaluationSample.UnitTest")]
+[assembly: System.Reflection.AssemblyTitleAttribute("EvaluationSample.UnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
